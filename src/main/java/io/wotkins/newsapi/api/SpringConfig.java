eb0c4b93a473776e58714759fb7c1bd4135d43be
Nikita Wotkins <nikitablackman@yandex.ru>
2021-07-24 15:22:18 +0300

    ver. 1.1

---
+++ src/main/java/io/wotkins/newsapi/api/SpringConfig.java
@@ -0,0 +1,9 @@
+package io.wotkins.newsapi.api;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootConfiguration
+@ComponentScan
+public class SpringConfig {
+}
