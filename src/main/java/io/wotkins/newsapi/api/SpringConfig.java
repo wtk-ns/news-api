aa3e83c398e07dd8fb8bd3d57256bf8f05b83699
Nikita Wotkins <nikitablackman@yandex.ru>
2021-07-24 15:27:35 +0300

    ver.1.2.1

--- src/main/java/io/wotkins/newsapi/api/SpringConfig.java
+++
@@ -1,9 +0,0 @@
-package io.wotkins.newsapi.api;
-
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootConfiguration
-@ComponentScan
-public class SpringConfig {
-}
--- src/test/java/io/wotkins/newsapi/NewsApiApplicationTests.java
+++ src/test/java/io/wotkins/newsapi/NewsApiApplicationTests.java
@@ -1,9 +1,10 @@
 package io.wotkins.newsapi;
 
+import io.wotkins.newsapi.api.NewsApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = NewsApiApplication.class)
 class NewsApiApplicationTests {
 
     @Test
